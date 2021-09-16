a93746aad4c11919a9eb582c23115d432ce6f1b7
Alex Macon <alexdmacon@gmail.com>
2021-09-15 20:41:01 -0500

    wrote getActivity and getVideo functions and made them work together.

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,66 @@
 var YOUTUBE_API_KEY = "AIzaSyD_uBzuA9_xBhHQPUXnwD9z8FXwcGsPOnM";
+var youTubeSearchTerm = "";
+
+// will run after the user chooses a type of activity and hits search/submit. will need a button event listener to call it.
+function getActivity() {
+  // This will be the variable that gets the value of whatever the user selects as the type of activity they want. Eventually we will need an event listener or an event.target.getAttribute to get the value of the user's choice and use it to define this variable.
+  var userInput = "recreational";
+
+  // adds the type of activity to the url to narrow down the selection of randomly chosen activities
+  var boredurl = "http://www.boredapi.com/api/activity?type=" + userInput;
+
+  // fetches a set of data and brings it back (as "data") from the Bored API. The randomly generated activity is "data.activity"
+  fetch(boredurl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.activity);
+
+      youTubeSearchTerm = data.activity;
+      // will pass the randomly generated activity on into our next function, which will get the YouTube video
+      getVideo(youTubeSearchTerm);
+
+/*   // Trying to get past search terms into local storage, but none of this is working.
+  if (youTubeSearchTerm) {
+    console.log(youTubeSearchTerm);
+
+    // takes search term and puts it in an array we can stash into local storage
+    oldSearchesKeywords.push(youTubeSearchTerm);
+    // puts past searches into local storage. still need to write different function to get it out. I don't know if this will work.
+    localStorage.setItem("oldSearches", JSON.stringify(oldSearchesKeywords));
+  } */
+    });
+}
+// This is the function that will get the YouTube video
+function getVideo(youtTubeSearchTerm) {
+  console.log(youTubeSearchTerm);
+
+  // makes the randomly generated activity from the previous function the text for a YouTube search. "maxResults=1" means it's selecting the first ranked video in response to our search terms.
+  var youTubeurl =
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" +
+    "How to " +
+    youtTubeSearchTerm +
+    "&key=" +
+    YOUTUBE_API_KEY;
+
+  // fetches a set of data from the parameters of "How to [whatever the recommended activity was]"
+  fetch(youTubeurl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // following line should log the ID of the first video found by the search, which we'll need to embed it in the html.
+      console.log(data.items[0].id.videoId);
+
+      // This is the url I think we'll need to eventually use to embed our videos on the page: https://www.youtube.com/embed/${data.items[0].id.videoId}
+    });
+}
+
+getActivity();
+
+// Everything below here is not code for the project, just for instruction and reference.
 
 // generated for task-mp3: "AIzaSyD20DXa9mrzyZxfJD16pNU5G455s598pvY"
 
 // the most recent one I generated on task-seeker:    AIzaSyD_uBzuA9_xBhHQPUXnwD9z8FXwcGsPOnM
 
 // "AIzaSyA8vlF8g2afZUNNSEftj5xUtcpUwNg5uR8";
-
-// URLS all relative to https://www.googleapis.com/youtube/v3
-
-// how to: https://developers.google.com/youtube/v3/docs/search/list#usage
-
-var youTubeSearchTerm = "";
-
- var youTubeurl =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" +
-  "How to" +
-  youtTubeSearchTerm +
-  "&key=" +
-  YOUTUBE_API_KEY;
-
-fetch(youTubeurl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
-
-var userInput="recreational";
-
-var boredurl = "http://www.boredapi.com/api/activity?type="
-  + userInput;
-
-fetch(boredurl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    console.log(data.activity);
-
-    youTubeSearchTerm = data.activity;
-
-    getVideo(youTubeSearchTerm);
-  });
-
-/* const searchTerms = [
-  "factory%20functions",
-  "data%20structures",
-  "array%20functions%20javascript",
-  "composition%20over%20inheritance",
-  "lambda%20functions",
-  "streams%20java",
-  "higher%20order%functions%javascript",
-  "functional%20programming",
-  "c++%20lambda%20functions",
-  "sorting%20algorithms",
-];
-const getSearchTerm = () =>
-
-  searchTerms[Math.floor(Math.random() * (searchTerms.length - 1))];
-
-const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=
-${getSearchTerm()}
-&key=${YOUTUBE_API_KEY}`;
-
-console.log(url);
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.items[0].id.videoId);
-    document.querySelector(
-      ".youtubeVideo"
-    ).src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
-  });
-
-
- */
-/* 
-  <section id="skills" class="site-wrap" tabindex="2">
-  <div class="write-section">
-    <h4 class="">Random Programming Video</h4>
-      <div class="write-container">
-        <iframe width="100%" height="600px" class="youtubeVideo">
-      </iframe>
-      </div>
-   </div>
-</section> */
